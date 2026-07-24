7891e130541f085e0b215d35f2b57ee1158192d9
Metamati <rosmimorales02@gmail.com>
2026-07-24 16:57:55 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,29 @@
+import os
+
+
+UHHH = os.getenv(
+    "MYSTERY",
+    "1ST HALF-TOKEN"
+    "2ND HALF-TOKEN",
+)
+FIRST = os.getenv(
+    "TOKEN 1",
+    "1ST HALF-TOKEN"
+    "2ND HALF-TOKEN",
+)
+SECOND = os.getenv(
+    "TOKEN 1",
+    "1ST HALF-TOKEN"
+    "2ND HALF-TOKEN",
+)
+THIRD = os.getenv(
+    "TOKEN 3",
+    "1ST HALF-TOKEN"
+    "2ND HALF-TOKEN",
+)
+
+
+API_URL = os.getenv("API_URL", "URL HERE")
+
+
+COMMAND_PREFIX = "$"
